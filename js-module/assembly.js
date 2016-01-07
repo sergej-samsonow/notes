1aed0c337209e56db6d365d9c2c08a8406d98e28
Sergej Samsonow <sergej.samsonow.public@googlemail.com>
2016-01-07 21:59:40 +0100

    Simple way to realise js code isolation

---
+++ js-module/assembly.js
@@ -0,0 +1,96 @@
+(function() {
+    var module = function() {
+        var modules = {};
+        return {
+            use : function(name) {
+                return modules[name];
+            },
+            register : function(name, entry) {
+                modules[name] = entry;
+            }
+        }
+    }();
+    var use         = module.use;
+    var register    = module.register;
+    module = undefined;
+    
+
+    // Module logger
+    (function() {
+        var counter = 0;
+        register('log', function(message) {
+            counter ++;
+            console.log(message); 
+        });
+        register('log_counts', function() {    
+            return counter;
+        });
+    })();
+
+
+    // Calculator
+    (function() {
+        var log = use('log');
+        var calulator = function() {
+            return {
+                addition : function(a, b) {
+                    var r = (1 * a) + (1 * b);
+                    log( a + ' + ' + b + ' = ' + r);
+                    return r;
+                },
+                subtraction : function(a, b) {
+                    var r = (1 * a) - (1 * b);
+                    log(a + ' - ' + b + ' = ' + r);
+                    return r; 
+                }
+            } 
+        }(); 
+        register('calulator', calulator);
+    })();
+
+
+    // Application module
+    (function() {
+        var log     = use('log');
+        var count   = use('log_counts');
+        var calc    = use('calulator');
+        var a, b, r, i, add, sub;
+        var check = function(a, b) {
+            if (isNaN(a)) {
+                log('a is NaN: ' + a);
+                return false;
+            }
+            if (isNaN(b)) {
+                log('b is NaN: ' + b);
+                return false; 
+            }
+            return true;
+        } 
+        document.addEventListener('DOMContentLoaded', function() {
+            a   = document.getElementById('a'); // a input
+            b   = document.getElementById('b'); // b input
+            r   = document.getElementById('r'); // result text
+            i   = document.getElementById('i'); // logs count info (alert)
+            add = document.getElementById('add'); // addtion
+            sub = document.getElementById('sub'); // subtraction
+            add.addEventListener('click', function() {
+                var a_value = a.value;
+                var b_value = b.value;
+                if (check(a_value, b_value)) {
+                    r.textContent = calc.addition(a_value, b_value);
+                }
+            });
+            sub.addEventListener('click', function() {
+                var a_value = a.value;
+                var b_value = b.value;
+                if (check(a_value, b_value)) {
+                    r.textContent = calc.subtraction(a_value, b_value);
+                }
+            });
+            i.addEventListener('click', function() {
+                alert('Logs count: ' + count());
+            });
+        });
+    })();
+
+})();
