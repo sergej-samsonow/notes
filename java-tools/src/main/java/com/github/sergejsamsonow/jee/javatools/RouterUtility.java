b1288f88743efdcb2a89ed8b8a25288970f723da
Sergej Samsonow <sergej.samsonow.public@googlemail.com>
2016-02-27 13:49:16 +0100

    Jee tools project

---
+++ java-tools/src/main/java/com/github/sergejsamsonow/jee/javatools/RouterUtility.java
@@ -0,0 +1,11 @@
+package com.github.sergejsamsonow.jee.javatools;
+
+public class RouterUtility {
+
+    public static String htmlPathToJsp(String path) {
+        return null;
+    }
+    public static String jspPathToHtml(String path) {
+        return null;
+    }
+}
